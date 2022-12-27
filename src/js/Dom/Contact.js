609840a58f63e23351c2af1d27b30e2d32135454
NestorMyrie <Myrienestor@gmail.com>
2022-12-26 23:28:37 -0600

    retoques de diseño contact.css

--- src/js/Dom/Contact.js
+++ src/js/Dom/Contact.js
@@ -1,19 +1,33 @@
-// const data = {
-//     service_id: 'service_fb7b0le',
-//     template_id: 'template_mynpcgb',
-//     user_id: 'user_Tm5AttXYzU5BuRTJWijHT',
-//     template_params: {
-//         'nombre': 'Nestor',
-//         'correo':"asdsd@gmail.com",
-//         "numero":"87878787",
-//         "mensaje":"Hola"
-//     }
-// };
+const form = document.getElementById('contact-form'
+)
+
+form.addEventListener('submit', handleSubmid)
+
+async function handleSubmid(event) {
+    event.preventDefault()
+const {name,mail,phone,mesage} =this
+if(name.value.length <=3){
+
+    name.parentNode.lastElementChild.style.border='1px solid red'
+}
+
+
+// const errormsg =document.getElementById('main_contact__form_error')
+// if(name.value.length <=3 || !isEmail(mail.value) || (typeof phone != 'number')){
+//     errormsg
+// }
  
-// fetch('https://api.emailjs.com/api/v1.0/email/send', {
-//     method: 'POST',
-//     body: JSON.stringify(data),
-//     headers:{
-//         'Content-Type': 'application/json'
-//     }
-// }).then(e=>console.log(e))
+ 
+
+//     const formData = new FormData(this)
+//    const response= await fetch(this.action,{
+//       method:this.method,
+//       body:formData,
+//       headers:{
+//           "Accept":"application/json"
+//       }
+//     })
+//   if(response.ok){
+//       console.log("se Envio!")
+//   }
+  }
