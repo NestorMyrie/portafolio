d26befed59502eb3be71a14c399ee04bf366f2c3
NestorMyrie <Myrienestor@gmail.com>
2022-12-30 17:21:52 -0600

    Modificacion de padding para dispositivos pequeños

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,8 @@ import './Dom/Services.js';
 import './Dom/Proyects.js';
 import './Dom/Contact.js';
 import './Dom/NavBar.js';
+import './Dom/Loader.js'
 
-AOS.init();
+AOS.init({
+    once:true
+});
